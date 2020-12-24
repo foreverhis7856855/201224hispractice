4c1a55368e9dbcfec23ca64c1a54539cd2e3438d
foreverhis7856 <foreverhis@naver.com>
2020-12-24 18:52:31 +0900

    코딩도장 7장 연습 year, hour 함수 등등

---
+++ chapter7.py
@@ -0,0 +1,18 @@
+Python 3.7.5 (default, Nov  7 2019, 10:50:52) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> year = 2020
+>>> month = 12
+>>> day = 24
+>>> hour = 18
+>>> minute = 50
+>>> second = 45
+>>> 
+>>> print(year, month, day, sep='/
+      
+SyntaxError: EOL while scanning string literal
+>>> print(year, month, day, sep='/')
+2020/12/24
+>>> print(hour, minute, second, sep=':')
+18:50:45
+>>> 
